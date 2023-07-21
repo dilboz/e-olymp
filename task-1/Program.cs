0974cd79592e9077a0497b835f2f13b912cabd22
dilboz <dilbozsafarov2002@gmail.com>
2023-07-21 16:47:03 +0500

    task-1

---
+++ task-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine() ?? string.Empty);
+            
+            Console.WriteLine($"{n/10} {n%10}");
+        }
+    }
+}
