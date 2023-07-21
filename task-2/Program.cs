38a2ee1712434cd2478b7f02152442f30dfa5b4f
dilboz <dilbozsafarov2002@gmail.com>
2023-07-21 16:53:10 +0500

    task-2

---
+++ task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine() ?? string.Empty);
+            
+            int count = GetDigitCount(number);
+            Console.WriteLine(count);
+
+            Console.Read();
+        }
+
+        private static int GetDigitCount(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            var count = 0;
+            while (number != 0)
+            {
+                number /= 10;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
